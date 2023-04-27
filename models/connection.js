4ab1f4f2e55e8bfcfbff5f4d32e4e67dc7bbedc9
Emilie <em_bntd3112@yahoo.com>
2023-04-27 18:08:03 +0200

    page login

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString = 'mongodb+srv://emiliebenistandpro:h1OqVkHZWg5cxYr1@cluster0.e4is5oz.mongodb.net/hackatweet'
+
+// process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
